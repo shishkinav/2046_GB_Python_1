5c09bc212170d552d92f3f23a8c1ed0ecf687700
Anatoliy Shishkin <shishkinanatoli@gmail.com>
2021-12-24 22:57:58 +0300

    пример кода с урока

---
+++ Shishkin_Anatoliy_lesson_1/code_1.py
@@ -0,0 +1,98 @@
+"""
+http://git-scm.com/book/ru/v2
+https://www.python.org/
+https://www.jetbrains.com/pycharm/download/#section=linux
+"""
+
+# print(5 + 6)
+# print(5 - 6)
+# print(5 * 6)
+# print(5 / 6)
+# print(37 // 6)
+# print(37 % 6)
+# print(2**5)
+
+# print(5 > 6)
+# print(5 < 6)
+# print(5 == 6)
+# print(5 != 6)
+# print(5 >= 6)
+# print(5 <= 6)
+
+# snake_case = 'то что мы хотим в него присвоить'
+# print(type(snake_case))
+# print(snake_case)
+# snake_case = 100.00
+# print(type(snake_case))
+# print(snake_case)
+
+# name = input("Введите Ваше имя:\n")
+# print(f"Привет, рад Вас видеть {name}")
+
+# int()
+# float()
+# str()
+# bool()
+
+# age = int(input("Введите Ваш возраст:\n"))
+# print(age // 2)
+
+
+
+# a = 0
+# b = 5
+# c = -9
+#
+# out = b > a and c < 0
+# print(out)
+# out1 = not((b > a or c < 0) and b == 5)
+# print(out1)
+
+user_password = '123'
+admin_password = 'qwerty'
+# passwd = input("Введите пароль входа в систему:\n")
+# if passwd == user_password:
+#     print("Пользователь авторизован!")
+#     adm_passwd = input("Пароль администратора:\n")
+#     if adm_passwd == admin_password:
+#         print("Да, босс, слушаю тебя!")
+#     else:
+#         print("Не угадал, доступа нет!")
+# else:
+#     print("Я Вас не знаю, идите дальше.")
+
+
+# color = 'green'
+# if color == 'green':
+#     print('зеленый')
+# elif color == 'red':
+#     print('красный')
+# elif color == 'blue':
+#     print('синий')
+# else:
+#     print('цвет неизвестен')
+
+
+# my_list = [1, '2', True, 'морковка']
+# print(my_list)
+
+# count = 5
+# while count > 0:
+#     print(count)
+#     count -= 1
+#     if count == 2:
+#         break
+
+numbers = [55, 66, 77]
+# for element in numbers:
+#     print(element)
+# for idx, element in enumerate(numbers, start=1):
+#     print(f"Индекс {idx} Значение {element}")
+
+# for el in range(1, 10, 2):
+#     print(el)
+
+for el in range(len(numbers)):
+    print(el)
+
+123
